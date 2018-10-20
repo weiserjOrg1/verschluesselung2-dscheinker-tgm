9ac61000c26954e79aaff3b43efc6883bd2751a9
Daniel Scheinker <dscheinker@student.tgm.ac.at>
2018-10-20 19:43:00 +0200

    Jetzt?

--- src/example/ExampleClass2.java
+++ src/example/ExampleClass2.java
@@ -3,7 +3,7 @@ package example;
 public class ExampleClass2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!!");
+		System.out.println("Hello world");
 		System.out.println("Hello world!!!");
 		System.out.println("Hello world!!!");
 		System.out.println("Hello world!!!");
