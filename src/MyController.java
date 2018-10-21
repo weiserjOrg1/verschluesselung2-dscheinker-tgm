fb0ad85faccbcf28deffb5b92e4e7d62a10469e6
Daniel Scheinker <dscheinker@student.tgm.ac.at>
2018-10-21 15:19:46 +0200

    Fertigstellung der Klasse: MyView(MVC)

--- src/MyController.java
+++ src/MyController.java
@@ -17,33 +17,33 @@ public class MyController implements ActionListener {
 
     @Override
     
-   // Klarstellung auf die Interaktion auf Aktionen/Events
-    
-    public void actionPerformed(ActionEvent e) {
-        if (v.getShiftRadioButton() == e.getSource()) {
-            v.getSecret().setEnabled(false);
-            v.getShift().setEnabled(true);
-        }
-        if (v.getSecretRadioButton() == e.getSource()) {
-            v.getShift().setEnabled(false);
-            v.getSecret().setEnabled(true);
-        }
-        if (v.getEncryptButton() == e.getSource()) {
-            if (this.v.getShiftRadioButton().isSelected()) {
-                m.setAbc(new ShiftCipher(Integer.parseInt(v.getShift().getText())));
-            } else {
-                m.setAbc(new SubstitutionCipher(v.getSecret().getText()));
-            }
+    // Klarstellung auf die Interaktion auf Aktionen/Events
+     
+     public void actionPerformed(ActionEvent e) {
+         if (v.getShiftRadioButton() == e.getSource()) {
+             v.getSecret().setEnabled(false);
+             v.getShift().setEnabled(true);
+         }
+         if (v.getSecretRadioButton() == e.getSource()) {
+             v.getShift().setEnabled(false);
+             v.getSecret().setEnabled(true);
+         }
+         if (v.getEncryptButton() == e.getSource()) {
+             if (this.v.getShiftRadioButton().isSelected()) {
+                 m.setAbc(new ShiftCipher(Integer.parseInt(v.getShift().getText())));
+             } else {
+                 m.setAbc(new SubstitutionCipher(v.getSecret().getText()));
+             }
 
-            this.v.getOutput().setText(this.m.getAbc().encrypt(this.v.getInput().getText()));
-           
-        }
-        if(v.getDecryptCheckBox().isSelected()) {
-        	this.v.getOutput().setText(this.m.getAbc().decrypt(this.v.getInput().getText()));
-        }
-    }
-    public static void main(String[] args) {
-        MyController c = new MyController();
-    }
-   
+             this.v.getOutput().setText(this.m.getAbc().encrypt(this.v.getInput().getText()));
+            
+         }
+         if(v.getDecryptCheckBox().isSelected()) {
+         	this.v.getOutput().setText(this.m.getAbc().decrypt(this.v.getInput().getText()));
+         }
+     }
+     public static void main(String[] args) {
+         MyController c = new MyController();
+     }
+    
 }
